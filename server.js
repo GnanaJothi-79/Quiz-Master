708b505823c6c8ddccd66abff8394b2638812f0c
GnanaJothi-79 <gnana07092004@gmail.com>
2025-12-22 12:55:52 +0530

    prepare

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-import jsonServer from "json-server";
+const jsonServer = require("json-server");
+const path = require("path");
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser);
 server.use(router);
 
 server.listen(PORT, () => {
-  console.log(`JSON Server running on port ${PORT}`);
+  console.log(`✅ JSON Server running on port ${PORT}`);
 });
